d23c8cf99f986d9fb4e3d8583b9491c05caf76a7
Prateek <prateek1440003@gmail.com>
2026-08-17 12:52:29 +0530

    feat: add realtime channel broadcasting

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
   res.send('Blockchain server is running');
+});
+
+io.on('connection', function(socket) {
+  socket.on('channel', function(msg) {
+    io.emit('channel', msg);
+  });
 });
 
 http.listen(4000, function() {
